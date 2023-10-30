09c394e06e1d3c764ecbeda5409c0003b54d5d56
Brenden Smith <brenden@brendensmith.net>
2023-10-30 08:52:35 -0500

    Custom error pages and GitHub pages config
    Add custom error pages
    Github pages config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<script lang="ts">
-</script>
-
 <div class="container w-auto h-auto flex flex-col items-center justify-center">
 	<a href="/board"><button class="btn btn-accent">Go to the notes board</button></a>
 </div>
