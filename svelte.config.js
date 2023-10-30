308dd847d2b79d484882b4aaa4c7280b9e4cd38e
Brenden Smith <brenden@brendensmith.net>
2023-10-30 12:32:24 -0500

    move config.paths to config.kit.paths where it should be

--- svelte.config.js
+++ svelte.config.js
@@ -6,18 +6,18 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess({})],
-	paths: {
-		base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-	},
 
 
 	kit: {
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
-			assets: 'public',
+			assets: 'build',
 			fallback: '404.html',
 			precompress: false,
 			strict: true,
